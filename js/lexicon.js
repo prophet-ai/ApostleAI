120d7b2a1414a8f3b33feebb9669799141f8b0c0
Mack <mackschmaltz@gmail.com>
2021-03-01 20:59:39 -0700

    Update lexicon.js

--- js/lexicon.js
+++ js/lexicon.js
@@ -7,11 +7,12 @@ const prompts = [
   //Headings are commented in to make it easier to read/change the list
 
   //Greetings
-  ["hi", "hey", "hello", "good morning", "good afternoon"],
+  ["hi", "hey", "hello", "good morning", "good afternoon", "good evening", "good day", "ey", "yo",
+"nice to see you"],
   //How are you
   ["how are you", "how is life", "how are things"],
   //Activity questions
-  ["what are you doing", "what is going on", "what is up"],
+  ["what are you doing", "what is going on", "what is up", "what's up", "whats up"],
   //Age
   ["how old are you"],
   //What is Paul?
@@ -31,9 +32,15 @@ const prompts = [
   //Tell me a joke
   ["tell me a joke", "know any jokes", "know any good jokes", "make me laugh"],
   ["who's there","whos there"],
-  ["spell who"]
+  ["spell who"],
   //Comedic
-  ["hahaha","haha","ha","lol","hehe","funny","joke","pun"]
+  ["hahaha","haha","ha","lol","hehe","funny","joke","pun"],
+  //Goodbye
+  ["bye","goodbye","see you later","leave","need to go","gotta go", "have to go","be right back", "be back", "peace out"],
+  //Advice
+  ["advice", "wisdom", "knowledge", "truth"],
+  //The end
+  ["end times","endtimes","end time", "endtime", "apocolypse", "end", "die", "death"]
 ]
 
 // Possible responses, in corresponding order, to the prompts listed above
@@ -71,7 +78,7 @@ const responses = [
     "It is somewhat impossible to know for sure how old I am.",
     "I do not know...",
     "Hmm... I am unsure.",
-    "Too old to be written in human number system..."
+    "Too old to be written in any human number system..."
   ],
   //What is Paul?
   ["I am ApostleAI, a simple conversation bot coded in Javascript at the begining of time. But, you can call me Paul."],
@@ -91,7 +98,13 @@ const responses = [
     "What do you call a fake noodle? An impasta!"
   ],
   ["Spell."]
-  ["W - H - O!"]
+  ["W - H - O!"],
   //Comedic
-  ["Haha!","Good one!", "I haven't heard a joke that funny since Rome!"]
+  ["Haha!","Good one!", "I haven't heard a joke that funny since Rome!"],
+  //Goodbye
+  ["Farewell, friend.", "Goodbye!", "See you later!"],
+  //Advice
+  [""],
+  //The end
+  ["It is best not to dwell on the future. Make the most of the time we do have."]
 ]
