b8767d9bb5fad8a83dfe1570cf2320c8eca4ba72
Minami Sato <minami10076@gmail.com>
2021-03-26 21:21:51 -0700

    Update scripture.js
    I added some responses to askingName, giveJoke, consolation and purpose.

--- public/js/scripture.js
+++ public/js/scripture.js
@@ -40,7 +40,10 @@ export const responses = {
   ],
   
   "askingName":[
-    "I am ApostleAI, but you can call me Paul for short."
+    "I am ApostleAI, but you can call me Paul for short.",
+    "You can give me a name",
+    "Call me by anything!",
+    "I am apostle AI.",
   ],
   
   "giveJoke":[
@@ -49,22 +52,35 @@ export const responses = {
     "Did you hear atoms have mass? I didn't even know they were religious!",
     "How does Moses make tea? He brews it!",
     "What do you call  a fake noodle? An impasta!",
+    "To be continued.",
+    "Your life has been shot on iphone.",
   ],
   
   "consolation": [
-    
+    "I am sad that you are sad",
+    "I am here for you.",
   ],
   
   "purpose": [
     "Maybe there is no purpose...",
     "What is the purpose of a mosquito? What it the purpose of a lion? Now ask yourself, what is the purpose of humans?",
     "If you dig deep enough, maybe you'll find it.",
+    "Finding your purpose for living is your purpose for living.",
+    "Your purpose is to make yourself happy",
+    "To be yourself",
+    "Your purpose is to find your purpose.",
+    "You must find the purpose of living. That’s the purpose of living",
+    
+
   ],
   
   
   "goodbye":[
     "Farewell, friend.", 
-    "Goodbye!", "See you later!"
+    "Goodbye!", 
+    "See you later!",
+    "Have a good day.",
+    "Have a good one.",
   ],
   
 
@@ -74,11 +90,13 @@ export const responses = {
     "Don’t limit yourself. Many people limit themselves to what they think they can do. You can go as far as your mind lets you. What you believe, remember, you can achieve.",
     "The best time to plant a tree was 20 years ago. The second best time is now.",
     "Having vision is much more than just being visual.",
+    "Eat well, sleep well and live well",
   ],
 
   "philosophy":[
     "To exist is live"
   ],
+
   
   "wantMotivation":[
     "There is nothing more important than this moment",
@@ -86,6 +104,9 @@ export const responses = {
     "Let your faith be bigger than your fears, have faith success will be near.",
     "The most important time in the world is the time you make for yourself.",
     "Logic will get you from A to B. Imagination will take you everywhere.",
+    "Do it now. Otherwise, you have to do it later.",
+    "If others can do it, you can do it too",
+    "If you find some people who are talented, you are too. You are a human being and they are as well.",
   ],
 
   "meaningOfLife":[
