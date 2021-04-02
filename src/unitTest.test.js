c59164e91b6c3bfaa71e9deb2c4a6cba49fabf68
Ben Wilson <ben@benmwilson.dev>
2021-04-01 20:12:40 -0700

    added new test

--- src/unitTest.test.js
+++ src/unitTest.test.js
@@ -1,5 +1,19 @@
 //import * as porterStemmingTest from "/js/porterStemming.js";
 const porterStemmingTest = require('./porterStemming');
+const Wit = require('node-wit').Wit;
+
+const client = new Wit(token, actions, logger);
+
+test('t', () =>{
+    expect(client.message('Hello', (error, data) => {
+        if (error) {
+          console.log('Oops! Got an error: ' + error);
+        } else {
+          console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
+        }
+      })
+      ).toMatch('hello');
+});
 
 //Tests if porter stemming is correcting words properly
 test('Porter Stemming Test', () =>{
