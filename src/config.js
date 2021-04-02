db2e99929013957a2eddb06fab5c9dc1f9533b73
Ben Wilson <ben@benmwilson.dev>
2021-04-01 21:12:11 -0700

    changes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // config for Wit API calls
 // put here so that the API key can be hidden and not publicly faced
 module.exports = {
-    key: 'KEY GOES HERE :)',
+    key: 'JPT6MY5E2CX4CJ2RXZ3JN3D7DIJS3Z4L',
 }
