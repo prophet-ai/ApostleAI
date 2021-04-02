1b5cab9368c3c680d77f35db1e7eba4993d6a844
NigamLad <nigamlad2012@gmail.com>
2021-04-01 20:59:54 -0600

    Update porterStemming.test.js

--- src/porterStemming.test.js
+++ src/porterStemming.test.js
@@ -3,11 +3,11 @@ const porterStemmingTest = require('./porterStemming');
 
 //Tests if porter stemming is correcting words properly
 test('Porter Stemming Test', () =>{
-    expect(porterStemmingTest("I like sceince")).toMatch('I like science.');
-    expect(porterStemmingTest("Testign")).toMatch('Testing.');
-    expect(porterStemmingTest("collectoin")).toMatch('collection.');
-    expect(porterStemmingTest("discoverign")).toMatch('discovering.');
-    expect(porterStemmingTest("fractoin")).toMatch('fraction.');
+    expect(porterStemmingTest.textInput("I like sceince")).toMatch('I like science.');
+    expect(porterStemmingTest.textInput("Testign")).toMatch('Testing.');
+    expect(porterStemmingTest.textInput("collectoin")).toMatch('collection.');
+    expect(porterStemmingTest.textInput("discoverign")).toMatch('discovering.');
+    expect(porterStemmingTest.textInput("fractoin")).toMatch('fraction.');
 });
 
 //Tests if bot is ouputting something and not throwing errors
